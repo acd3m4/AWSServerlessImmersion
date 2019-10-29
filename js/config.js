d60acbe0c5cca970f86ec5fb2264247baa8411aa
acd3m4 <acd3m4@mst.edu>
2019-10-29 16:39:13 +0000

    updated config

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-west-2_HUjLOBW4z', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 7s8diacdfkl6fkmq0cvmdn2rio'', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '7s8diacdfkl6fkmq0cvmdn2rio', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
